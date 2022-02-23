26da9d70d79d03ebd45353c0ff89046855082531
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-02-23 20:01:46 +0100

    Ej 11

--- Ejercicio 11.py
+++ Ejercicio 11.py
@@ -14,3 +14,9 @@ def mcd_sumas_restas(a, b):
         b=aux
         a=aux
     return a
+
+def iniciar():
+    num1=5
+    num2=10
+    print("Por el algoritmo de Euclides: ".format(mcd_euclides(num1, num2)))
+    print("Por sumas y restas: ", mcd_sumas_restas(num1, num2))
