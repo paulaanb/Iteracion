4dc5e27c85492e35d85a9b86f0f960a7736ab5a0
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-02-23 19:39:30 +0100

    Ej 9

--- Ejercicio 9.py
+++ Ejercicio 9.py
@@ -1,2 +1,9 @@
 #Ejercicio 9: Búsqueda de palabras en un diccionario
+import tabulate
 
+def tabla(diccionario):
+    table=[]
+    for i in range(0, len(diccionario)):
+        table.append(list(diccionario[i:i+1]))
+    headders=["i", "diccionario", "anterior", "siguiente"]
+    print(tabulate.tabulate(table, tablefmt="fancy_gird", showindex=True))
