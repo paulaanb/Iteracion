7c99d759b2e78c0d67dc6422848865099c66c021
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-02-23 19:27:09 +0100

    Ej 7

--- Ejercicio 7.py
+++ Ejercicio 7.py
@@ -1,9 +1,16 @@
 #Ejercicio 7: Edición de un número entero
 base=int(input("Introduce la base mayor o igual a 2: "))
+numero=int(input("Escriba un numero: "))
 
-def (numero, base):
+def edit(numero, base):
     if base > 36:
-        print(número)
+        print(numero)
     elif base < 2:
         print("Esta base no esta permitida")
-    
+    else:
+        if numero//base==0:
+            print(numero%base)
+        else:
+            numero = numero//base
+
+edit(numero, base)
