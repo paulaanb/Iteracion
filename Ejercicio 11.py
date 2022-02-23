28c323eecddec97d1ef787688f035149614bd37c
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-02-23 19:58:33 +0100

    Ej 11

--- Ejercicio 11.py
+++ Ejercicio 11.py
@@ -1,2 +1,16 @@
 #Ejercicio 11: mcd de dos números enteros
 
+def mcd_euclides(a, b):
+    while b != 0:
+        #Utilizo propiedad: mcd(a, b)=mcd(a-b, b)
+        aux=b
+        b=a%b
+        a=aux
+    return a
+
+def mcd_sumas_restas(a, b):
+    while b!=0:
+        aux=b-a
+        b=aux
+        a=aux
+    return a
